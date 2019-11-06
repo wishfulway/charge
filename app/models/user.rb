a912edd440b206831febb88be55889dacf434c23
jiangnan <jiangn8866@163.com>
2019-11-06 11:00:17 +0800

    首次提交 注册功能已实现

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+
+  # rails g devise user
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+
+
+end
